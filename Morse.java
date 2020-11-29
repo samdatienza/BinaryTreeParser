a3921fddd0bab3d3d8ebb4762f3472e40c806318
samdatienza <71737101+samdatienza@users.noreply.github.com>
2020-11-29 17:08:19 -0600

    Add files via upload

--- Morse.java
+++ Morse.java
@@ -7,14 +7,17 @@ public class Morse {
 	
 	public static void main(String[] args) throws IOException {
 		
+		//Create scanner to get string to convert to Morse Code
 		Scanner scanner = new Scanner(System.in);
+		
 		//Get user input string for conversion
 		System.out.println("Please enter word(s) to be converted to Morse Code: ");
 		
 		String text = scanner.nextLine();
 		
+		//Iterate through each character and convert to Morse equivalent
 		for (int i = 0; i < text.length(); i++) {
-			//Morse code conversion method
+			//I'm thinking we can write a Morse code conversion method then call it here and print result to console all in this loop
 		}
 		
 		
--- MorseNode.java
+++ MorseNode.java
@@ -2,17 +2,17 @@ package sem2project3;
 
 public class MorseNode {
 	
-	public char letter;
+	public char letter;			//Letter held in node
 	
-	public MorseNode leftMorse, rightMorse;
+	public MorseNode leftMorse, rightMorse;				//Nodes to left and right
 	
 	public MorseNode(char theLetter)
 	{letter = theLetter; }
 	
 	public MorseNode(char theLetter, MorseNode left, MorseNode right) 
-	{letter = theLetter;
-	leftMorse = left;
-	rightMorse = right;
+	{letter = theLetter;		//Letter held in node
+	leftMorse = left;			//MorseNode to left
+	rightMorse = right;			//MorseNode to right
 	}
 
 }

1a1c8ed20c6d1854cefa28c247d9e53453407897
Swolf1997 <71954090+Swolf1997@users.noreply.github.com>
2020-11-30 11:14:59 -0600

    Add files via upload

---
+++ Project_3/src/project_3/Main.java
@@ -0,0 +1,21 @@
+package project_3;
+
+import java.io.FileInputStream;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// File input {inputFile} and using {reader} to read from file.
+		FileInputStream inputFile = new FileInputStream("expression.txt");
+		Scanner reader = new Scanner(inputFile);
+		
+		while (reader.hasNext()) {
+			// Will read from file and adding spaces of {expression} at the same time if need be
+			String expression = AddSpaces.addSpaces(reader.nextLine());
+		}
+		
+		// Closing {reader} and {inputFile}
+		reader.close();
+		inputFile.close();
+	}
+}

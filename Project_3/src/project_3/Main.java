14490d0a7c77e8652eb6166ff5341eec081cec76
samdatienza <71737101+samdatienza@users.noreply.github.com>
2020-12-01 11:44:22 -0600

    Update Main.java

--- Project_3/src/project_3/Main.java
+++ Project_3/src/project_3/Main.java
@@ -3,6 +3,52 @@ package project_3;
 import java.io.FileInputStream;
 import java.util.Scanner;
 
+
+/*
+	//Build tree method
+	public static treeNode buildTree(String expression) {
+		
+		//Turn expression into array of operands/operators
+		String[] newExp = expression.split(" ");
+		
+		int len = newExp.length;
+		
+		
+		//Empty stack to push nodes of tree to
+		Stack<treeNode> nodeStack = new Stack<treeNode>();
+		
+		for (int i = 0; i < len; i++) {
+			if( reader.next() == "^" ||
+				reader.next() == "*" ||
+				reader.next() == "/" ||
+				reader.next() == "%" ||
+				reader.next() == "+" ||
+				reader.next() == "-" ||
+				reader.next() == ">" ||
+				reader.next() == ">=" ||
+				reader.next() == "<" ||
+				reader.next() == "<=" ||
+				reader.next() == "==" ||
+				reader.next() == "!=" ||
+				reader.next() == "&&" ||
+				reader.next() == "||" ) {
+				//Create new treenode containing operator
+				
+				//New treeNode^.nodeRight = nodeStack.pop();
+				//New treeNode^.nodeLeft = nodeStack.pop();
+				
+				//nodeStack.push(new treeNode^)
+			}
+			else {					//If not operator, must be operand
+				//Create new treeNode containing operand
+				//nodeStack.push(new  treeNode^);
+			}
+				
+		}
+		
+		
+	}
+	*/
 public class Main {
 	public static void main(String[] args) throws Exception {
 		// Scanner reads from user input string

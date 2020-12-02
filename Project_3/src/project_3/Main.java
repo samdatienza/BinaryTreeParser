bc5505d478f96fc8fd730d440eb2a0f7beb22c66
samdatienza <71737101+samdatienza@users.noreply.github.com>
2020-12-01 21:41:00 -0600

    Update Main.java

--- Project_3/src/project_3/Main.java
+++ Project_3/src/project_3/Main.java
@@ -4,54 +4,7 @@ import java.util.*;
 
 // Using https://www.sanfoundry.com/java-program-construct-expression-tree-infix-expression/ for help
 
-/*
-	//Build tree method
-	public static treeNode buildTree(String expression) {
-		
-		//Turn expression into array of operands/operators
-		String[] newExp = expression.split(" ");
-		
-		int len = newExp.length;
-		
-		
-		//Empty stack to push nodes of tree to
-		Stack<treeNode> nodeStack = new Stack<treeNode>();
-		
-		for (int i = 0; i < len; i++) {
-			if( reader.next() == "^" ||
-				reader.next() == "*" ||
-				reader.next() == "/" ||
-				reader.next() == "%" ||
-				reader.next() == "+" ||
-				reader.next() == "-" ||
-				reader.next() == ">" ||
-				reader.next() == ">=" ||
-				reader.next() == "<" ||
-				reader.next() == "<=" ||
-				reader.next() == "==" ||
-				reader.next() == "!=" ||
-				reader.next() == "&&" ||
-				reader.next() == "||" ) {
-				treeNode newNode;
-				//Line to make newNode contain operator
-				
-				
-				newNode.nodeRight = nodeStack.pop();
-				newNode.nodeLeft = nodeStack.pop();
-				
-				nodeStack.push(newNode);
-			}
-			else {					//If not operator, must be operand
-				treeNode oprNode;//Create new treeNode containing operand
-				//Line to make oprNode contain operator
-				nodeStack.push(oprNode);
-			}
-				
-		}
-		
-		
-	}
-	*/
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		// Scanner reads from user input string
